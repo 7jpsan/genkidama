1ff00d58e4f62cc8326e9b1fd12945e2166a2133
psoares-resilient <p.soares@resilientplc.com>
2018-06-14 12:12:47 +0100

    Add example of usage for (a)symmetric encryption using the library

---
+++ example/buffer-utils.ts
@@ -0,0 +1,25 @@
+import { Stream, Readable } from "stream";
+
+export class BufferUtils {
+  public static toStream(data: Buffer): Stream {
+    const r = new Readable();
+    r.push(data);
+    r.push(null);
+    return r;
+  }
+
+  public static async streamToBuffer(stream: Stream) {
+    return new Promise((resolve: (data: Buffer) => void, reject) => {
+      const chunks: Buffer[] = [];
+      let finalResult: Buffer;
+
+      stream.on('data', (chunk: string) => {
+        chunks.push(Buffer.from(chunk));
+      }).on('end', () => {
+        finalResult = Buffer.concat(chunks);
+        resolve(finalResult);
+      });
+    });
+  }
+
+}
---
+++ example/encrypt-asymmetric.ts
@@ -0,0 +1,15 @@
+import { CryptoManager } from '../lib';
+import keypair from 'keypair';
+
+// You can provide your own key pair by loading a file with fs or getting it from somewhere else...
+const keyPair = keypair({bits: 1024});
+const dataToEncrypt = "Maybe you won't be such a disappointment in the next dimension";
+
+// Asymmetric Encryption: 
+const encryptedResult = CryptoManager.Aes256Ctr().encryptAsym(keyPair.public, dataToEncrypt);
+const decryptedResult = CryptoManager.Aes256Ctr().decryptAsym(keyPair.private, encryptedResult);
+console.log(`
+  Original  (b64): ${Buffer.from(dataToEncrypt).toString('base64')}
+  Encrypted (b64): ${encryptedResult.toString('base64')}
+  Decrypted:       ${decryptedResult.toString()}
+`);
